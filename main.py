0b2b78dfe9eccbe37f91040c16a7670cb9bd3eba
Jose Antonio Mendoza <josmen@gmail.com>
2020-06-16 16:58:42 +0200

    Patrones de renombrado

--- main.py
+++ main.py
@@ -1,24 +1,55 @@
 import subprocess
+import re
+from datetime import datetime
 
-info_dict = {}
 exif_tool_path = 'exiftool'
 image_path = './images/2020-05-19__145947.JPG'
+camara = None
+fecha = None
 
-process = subprocess.Popen(
-        [exif_tool_path, image_path],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        universal_newlines=True)
+def metadatos(imagen):
+    """Devuelve el modelo de cámara y la fecha original
+    """
+    process = subprocess.Popen(
+            [exif_tool_path, imagen],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True)
 
-for tag in process.stdout:
-    line = tag.strip().split(':', 1)
-    if line[0].strip() == 'Camera Model Name':
-        print('\n' + 30 * '-')
-        print(f'{line[0].strip()} -> {line[-1].strip()}')
-    if line[0].strip() == r'Date/Time Original':
-        print(f'{line[0].strip()} -> {line[-1].strip()}')
-        print(30 * '-', '\n')
-    info_dict[line[0].strip()] = line[-1].strip()
+    for tag in process.stdout:
+        line = tag.strip().split(':', 1)
+        if line[0].strip() == 'Camera Model Name':
+            camara = line[-1].strip()
+        if line[0].strip() == r'Date/Time Original':
+            fecha = line[-1].strip()
+            fecha = fecha[:10].replace(':', '-')
 
-for k, v in info_dict.items():
-    print(k, ':', v)
+    # print(f'Camara: {camara}')
+    # print(f"Fecha: {fecha}")
+    return camara, fecha
+
+def renombra(imagen):
+    imagen = imagen.lower()
+    patron1 = re.compile(
+            r'''                # busca ./images/2020-05-19__145947.jpg
+            ^(.*/)              # ruta
+            (\d{4}-\d{2}-\d{2}) # fecha
+            (__)                # guiones
+            (\d{4,6}?)$         # contador
+            (.jpg)              # extension
+            ''', re.VERBOSE)
+
+    patron2 = re.compile(
+            r'''                # busca ./images/2014.12.19 - X-E2 - 0007.jpg
+            ^(.*/)              # ruta ./images/
+            (\d{4}\.\d{2}\.\d{2}) # fecha 2014.12.19
+            (\s-.*-\s)          # camara ' - X-E2 - '
+            (\d{4,6}?)          # contador
+            (.jpg)              # extension
+            ''', re.VERBOSE)
+
+    return nombre_sin_ext
+
+if __name__ == "__main__":
+    print(metadatos(image_path))
+    print(renombra(image_path))
